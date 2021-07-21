f728cd15beced3184ea53d0f03078a37933b3267
zX3no <dr_xeno@hotmail.com.au>
2021-07-21 17:21:02 +0930

    removed multithreading because it made things slower

--- src/main.rs
+++ src/main.rs
@@ -8,42 +8,18 @@ use std::thread;
 use std::time::Instant;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
-static GLOBAL_THREAD_COUNT: AtomicUsize = AtomicUsize::new(0);
 
 fn main() -> Result<()> {
     let now = Instant::now();
-    let file = Arc::new(Mutex::new(File::create("files.db").unwrap()));
-    for entry in WalkDir::new("../../")
-        .max_depth(1)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        let file = Arc::clone(&file);
 
-        if entry.file_type().is_dir() {
-            thread::spawn(move || {
-                GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
-                let mut file = file.lock().unwrap();
-                for x in WalkDir::new(entry.path())
-                    .into_iter()
-                    .filter_map(|e| e.ok())
-                {
-                    //WTF?
-                    let temp = x.file_name().to_string_lossy() + "\n";
-                    file.write_all(temp.as_bytes()).ok();
-                }
+    let mut file = File::create("files.db")?;
 
-                GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
-            });
-        } else {
-            let mut file = file.lock().unwrap();
-            let temp = entry.file_name().to_string_lossy() + "\n";
-            file.write_all(temp.as_bytes()).ok();
-        }
+    for entry in WalkDir::new("../../").into_iter().filter_map(|e| e.ok()) {
+        let temp = entry.file_name().to_string_lossy() + "\n";
+        file.write_all(temp.as_bytes())?;
     }
 
     //wait for all threads to finish
-    while GLOBAL_THREAD_COUNT.load(Ordering::SeqCst) != 0 {}
 
     println!("{}", now.elapsed().as_millis());
 
