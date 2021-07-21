61533ce6c96d5f6f12a98f12ac6d05ea512f877a
zX3no <dr_xeno@hotmail.com.au>
2021-07-21 16:56:22 +0930

    improved performance

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ static GLOBAL_THREAD_COUNT: AtomicUsize = AtomicUsize::new(0);
 fn main() -> Result<()> {
     let now = Instant::now();
     let file = Arc::new(Mutex::new(File::create("files.db").unwrap()));
-
     for entry in WalkDir::new("../../")
         .max_depth(1)
         .into_iter()
@@ -25,31 +24,28 @@ fn main() -> Result<()> {
             thread::spawn(move || {
                 GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
                 let mut file = file.lock().unwrap();
-
                 for x in WalkDir::new(entry.path())
                     .into_iter()
                     .filter_map(|e| e.ok())
                 {
-                    //this is super slow
-                    //maybe move data into vector than write it all at once later on
-                    file.write_all(x.file_name().to_string_lossy().as_bytes())
-                        .ok();
-                    file.write_all(b"\n").ok();
+                    //WTF?
+                    let temp = x.file_name().to_string_lossy() + "\n";
+                    file.write_all(temp.as_bytes()).ok();
                 }
 
                 GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
             });
         } else {
             let mut file = file.lock().unwrap();
-            file.write_all(entry.file_name().to_string_lossy().as_bytes())?;
-            file.write_all(b"\n")?;
+            let temp = entry.file_name().to_string_lossy() + "\n";
+            file.write_all(temp.as_bytes()).ok();
         }
     }
 
     //wait for all threads to finish
     while GLOBAL_THREAD_COUNT.load(Ordering::SeqCst) != 0 {}
 
-    println!("{}", now.elapsed().as_secs());
+    println!("{}", now.elapsed().as_millis());
 
     Ok(())
 }
