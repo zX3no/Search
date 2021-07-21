17f200c0a057e8eb46c6252f19c38fe37d8379ad
zX3no <dr_xeno@hotmail.com.au>
2021-07-21 16:16:31 +0930

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,6 @@ use walkdir::DirEntry;
 use walkdir::WalkDir;
 static GLOBAL_THREAD_COUNT: AtomicUsize = AtomicUsize::new(0);
 
-fn recurse(dir: DirEntry) -> Result<()> {
-    GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
-
-    // println!("Thread started scanning: {:?}", dir);
-    for entry in WalkDir::new(dir.path())
-        .max_depth(2)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        // file.write_all(entry.file_name().to_string_lossy().as_bytes())?;
-        // file.write_all(b"\n")?;
-    }
-    GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
-
-    Ok(())
-}
-
 fn main() -> Result<()> {
     let now = Instant::now();
     let file = Arc::new(Mutex::new(File::create("files.db").unwrap()));
@@ -44,10 +27,11 @@ fn main() -> Result<()> {
                 let mut file = file.lock().unwrap();
 
                 for x in WalkDir::new(entry.path())
-                    .max_depth(2)
                     .into_iter()
                     .filter_map(|e| e.ok())
                 {
+                    //this is super slow
+                    //maybe move data into vector than write it all at once later on
                     file.write_all(x.file_name().to_string_lossy().as_bytes())
                         .ok();
                     file.write_all(b"\n").ok();
@@ -55,6 +39,10 @@ fn main() -> Result<()> {
 
                 GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
             });
+        } else {
+            let mut file = file.lock().unwrap();
+            file.write_all(entry.file_name().to_string_lossy().as_bytes())?;
+            file.write_all(b"\n")?;
         }
     }
 
