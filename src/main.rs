e82683baf66f65b7dc2c2ce5405c328b1c764cf0
zX3no <dr_xeno@hotmail.com.au>
2021-07-21 16:00:35 +0930

    mutex

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,64 @@ use rayon::prelude::*;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Instant;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
+static GLOBAL_THREAD_COUNT: AtomicUsize = AtomicUsize::new(0);
 
-fn recurse(dir: &str, file: &File) -> Result<()> {
-    for entry in WalkDir::new(dir)
-        .max_depth(1)
+fn recurse(dir: DirEntry) -> Result<()> {
+    GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
+
+    // println!("Thread started scanning: {:?}", dir);
+    for entry in WalkDir::new(dir.path())
+        .max_depth(2)
         .into_iter()
         .filter_map(|e| e.ok())
     {
-        file.write_all(entry.file_name().to_string_lossy().as_bytes())?;
-        file.write_all(b"\n")?;
+        // file.write_all(entry.file_name().to_string_lossy().as_bytes())?;
+        // file.write_all(b"\n")?;
     }
+    GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
 
     Ok(())
 }
 
 fn main() -> Result<()> {
     let now = Instant::now();
-    //let file = File::create("files.db")?;
+    let file = Arc::new(Mutex::new(File::create("files.db").unwrap()));
 
-    for entry in WalkDir::new("../../../../")
+    for entry in WalkDir::new("../../")
         .max_depth(1)
         .into_iter()
         .filter_map(|e| e.ok())
     {
-        thread::spawn(move || {
-            recurse(&entry.file_name().to_string_lossy(), &file).ok();
-        });
+        let file = Arc::clone(&file);
+
+        if entry.file_type().is_dir() {
+            thread::spawn(move || {
+                GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
+                let mut file = file.lock().unwrap();
+
+                for x in WalkDir::new(entry.path())
+                    .max_depth(2)
+                    .into_iter()
+                    .filter_map(|e| e.ok())
+                {
+                    file.write_all(x.file_name().to_string_lossy().as_bytes())
+                        .ok();
+                    file.write_all(b"\n").ok();
+                }
+
+                GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
+            });
+        }
     }
 
     //wait for all threads to finish
+    while GLOBAL_THREAD_COUNT.load(Ordering::SeqCst) != 0 {}
 
     println!("{}", now.elapsed().as_secs());
 
